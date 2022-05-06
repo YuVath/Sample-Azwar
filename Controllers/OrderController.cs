df44a9802f78306021daefe4824d1429dfe852c5
13bsc <13bsc@DESKTOP-I4H26AO>
2022-05-06 20:05:31 +0500

    Add project files.

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sample.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace Sample_Azwar.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        
+
+        private readonly ILogger<OrderController> _logger;
+        private readonly IRequestClient<SubmitOrder> _submitOrderRequestClient;
+
+        public OrderController(ILogger<OrderController> logger, IRequestClient<SubmitOrder> submitOrderRequestClient)
+        {
+            _logger = logger;
+            _submitOrderRequestClient = submitOrderRequestClient;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Guid id, string customerNumber)
+        {
+            var (accepted, rejected) = await _submitOrderRequestClient.GetResponse<OrderSubmissionAccepted, OrderSubmissionRejected>(new
+            {
+
+                OrderId = id,
+                InVar.Timestamp,
+                CustomerNumber = default(string)
+            });
+
+            if (accepted.IsCompletedSuccessfully)
+            {
+                var response = await accepted;
+                return Ok(response);
+            }
+            else
+            {
+                var response = await rejected;
+                return BadRequest(response.Message);
+            }
+
+        }
+    }
+}
